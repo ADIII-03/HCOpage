39f1c3051a26c984efcd0ac3f92271e59dd8b6e8
ADIII-03 <aradityaraman0518@gmail.com>
2025-05-15 11:21:58 +0530

    ll

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import App from './App'
 
-
-
-function Layout() {
+export default function Layout() {
   return (
-    
-    <>
-    <Header />
-    <Outlet />
-    <Footer />
-    
-    </>
+    <App>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </App>
   )
 }
-
-export default Layout
