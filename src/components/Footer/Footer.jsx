30f422f23f51014cb0b97d182a2179c6bc4ac891
ADIII-03 <aradityaraman0518@gmail.com>
2025-05-15 11:46:39 +0530

    ll

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -79,10 +79,10 @@ export default function Footer() {
               <h2 className="mb-6 text-sm font-semibold uppercase">Legal</h2>
               <ul className="font-medium space-y-2">
                 <li>
-                  <Link to="#" className="hover:underline">Privacy Policy</Link>
+                  <Link to="/privacy-policy" className="hover:underline">Privacy Policy</Link>
                 </li>
                 <li>
-                  <Link to="#" className="hover:underline">Terms &amp; Conditions</Link>
+                  <Link to="/terms-and-conditions" className="hover:underline">Terms &amp; Conditions</Link>
                 </li>
               </ul>
             </div>
